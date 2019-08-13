97b3c0e01ef96e2e75a077ab7253991525661974
Max Matthews <max@mzmtech.com>
2019-08-12 21:45:42 -0400

    Abstract contact page to it's own component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import card from '../assets/MatthewsMaxCard.pdf'
+import Contact from './Contact'
 
 function encode(data) {
 	return Object.keys(data)
@@ -105,121 +105,14 @@ class Main extends React.Component {
 					{close}
 				</article>
 
-				<article
-					id="contact"
-					className={`${this.props.article === 'contact' ? 'active' : ''} ${
-						this.props.articleTimeout ? 'timeout' : ''
-					}`}
-					style={{ display: 'none' }}
-				>
-					<h2 className="major">Contact</h2>
-					{this.state.formSubmitted ? (
-						<p>Thanks, I'll be in touch soon!</p>
-					) : (
-						<form
-							name="contact"
-							method="POST"
-							data-netlify="true"
-							data-netlify-honeypot="bot-field"
-							onSubmit={this.handleSubmit}
-						>
-							<input type="hidden" name="form-name" value="contact" />
-							<div style={{ display: 'none' }}>
-								<label>Don’t fill this out if you're human: </label>
-								<input name="bot-field" onChange={this.handleChange} />
-							</div>
-							<div className="field half first">
-								<label htmlFor="name">Name</label>
-								<input
-									type="text"
-									name="name"
-									id="name"
-									onChange={this.handleChange}
-								/>
-							</div>
-							<div className="field half">
-								<label htmlFor="email">Email</label>
-								<input
-									type="text"
-									name="email"
-									id="email"
-									onChange={this.handleChange}
-								/>
-							</div>
-							<div className="field">
-								<label htmlFor="message">Message</label>
-								<textarea
-									name="message"
-									id="message"
-									rows="4"
-									onChange={this.handleChange}
-								/>
-							</div>
-							<ul className="actions">
-								<li>
-									<input
-										type="submit"
-										value="Send Message"
-										className="special"
-									/>
-								</li>
-							</ul>
-						</form>
-					)}
-					<ul className="icons">
-						<li>
-							<a
-								href="https://linkedin.com/in/maxzmatthews"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="icon fa-linkedin"
-							>
-								<span className="label">LinkedIn</span>
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://twitter.com/teckno101"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="icon fa-twitter"
-							>
-								<span className="label">Twitter</span>
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://medium.maxmatthe.ws"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="icon fa-rss"
-							>
-								<span className="label">Blog</span>
-							</a>
-						</li>
-						<li>
-							<a
-								href="https://github.com/maxmatthews/personal-site"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="icon fa-github"
-							>
-								<span className="label">GitHub</span>
-							</a>
-						</li>
-						<li>
-							<a
-								href={card}
-								target="_blank"
-								rel="noopener noreferrer"
-								className="icon fa-address-card"
-							>
-								<span className="label">Business Card</span>
-							</a>
-						</li>
-					</ul>
-					{close}
-				</article>
+				<Contact
+					article={this.props.article}
+					articleTimeout={this.props.articleTimeout}
+					formSubmitted={this.state.formSubmitted}
+					onSubmit={this.handleSubmit}
+					onChange={this.handleChange}
+					close={close}
+				/>
 			</div>
 		)
 	}
