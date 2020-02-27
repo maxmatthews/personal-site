de949693c17536f844aa9716ee2dfb5af34da2e1
Max Matthews <max@mzmtech.com>
2020-02-27 14:31:50 -0500

    Update content on get online workshop page

--- src/components/GetOnlineWorkshop.js
+++ src/components/GetOnlineWorkshop.js
@@ -11,17 +11,62 @@ export default class Credits extends Component {
 				style={{ display: 'none' }}
 			>
 				<h2 className="major">Get Online Workshop</h2>
-				<p>Click on any of the links below:</p>
-				<p>
-					Backgrounds by{' '}
-					<a
-						href="https://www.oxygenna.com/freebies/50-material-design-backgrounds"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Oxygenna
-					</a>
-				</p>
+				<ul>
+					<li>
+						<a
+							href="https://maxmatthe.ws/domain"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Domains from Namecheap
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://code.visualstudio.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							VS Code Text Editor
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://desktop.github.com/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Github Desktop
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://maxmatthe.ws/basicwebpage"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							HTML & CSS Code
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://youtu.be/aezevMkWQ_0"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Youtube Video
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://docs.google.com/document/d/1JeAMYNkpbJhK5e91Nx4eDln3KH7DG9z3BTj78cgSYzs/edit"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Vocab Sheet
+						</a>
+					</li>
+				</ul>
 				{this.props.close}
 			</article>
 		);
